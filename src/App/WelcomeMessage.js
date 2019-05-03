bff2dc312e46eeeab11fa4f8efb6b13811c07c66
Christopher Trauco <chris@trau.co>
2019-05-03 12:42:40 -0400

    created basic WelcomeMessage component and imported to 'App/index.js

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function Welcome() {
+    return <h1>Welcome to the Cryptocurrency Dashboard!</h1>;
+  }
+
+  export default Welcome;
---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import WelcomeMessage from './WelcomeMessage';
+
+class App extends React.Component {
+    render()  {
+    return (
+      <WelcomeMessage/>
+    );
+  }
+}
+
+export default App;
