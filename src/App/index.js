30ba26f06d15094e55f00bfbd5fab51e0096eaa5
Christopher Trauco <chris@trau.co>
2019-05-04 16:10:55 -0400

    Added Do Hyeon Google Fonts

--- src/App/index.js
+++ src/App/index.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 import WelcomeMessage from './WelcomeMessage';
+//#7 @ 03:13
+import styled, { css }from 'styled-components';
+
 
 class App extends React.Component {
-    render()  {
+  render()  {
     return (
+      <div>
       <WelcomeMessage/>
+      </div>
     );
   }
 }
