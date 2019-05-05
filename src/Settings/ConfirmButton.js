e48b7cd344c76c8c7a0af44db43d8148ae356924
Christopher Trauco <chris@trau.co>
2019-05-05 13:08:45 -0400

    Settings page, localStorage

---
+++ src/Settings/ConfirmButton.js
@@ -0,0 +1,28 @@
+// # 13 @ 05:00
+import React from 'react';
+import styled from 'styled-components';
+import { AppContext } from '../App/AppProvider';
+
+const ConfirmButtonStyled = styled.div `
+    margin: 20px;
+    color: green;
+    cursor: pointer;
+`
+export const CenterDiv = styled.div `
+    display: grid;
+    justify-content: center;
+    `
+
+
+
+export default function () {
+    return <AppContext.Consumer>
+        {({confirmFavorites}) =>
+        <CenterDiv>
+            <ConfirmButtonStyled onClick={confirmFavorites}> 
+            Confirm Favorites
+            </ConfirmButtonStyled>
+        </CenterDiv> 
+        }
+    </AppContext.Consumer>;
+}
