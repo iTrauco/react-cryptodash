b7070a6adad54f11b502afb7e950aa24a3e901b6
Christopher Trauco <chris@trau.co>
2019-05-07 15:26:04 -0400

    created CoinGrid.js with highlighted button styles

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,18 +1,20 @@
 // # 17 whole segment
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import { AppContext } from '../App/AppProvider';
-import App from '../App';
+import {SelectableTile} from '../Shared/Tile';
 
 export const CoinGridStyled = styled.div `
     display: grid;
     grid-template-columns: repeat(5, 1fr);
+    grid-gap: 15px;
 `
-export default function () {
+export default function() {
     return <AppContext.Consumer>
         {({coinList}) => <CoinGridStyled>
-           {Object.keys(coinList).map(coinKey =>
-            <div> {coinKey} </div>)} 
+            {Object.keys(coinList).map(coinKey => 
+           <SelectableTile>{coinKey}</SelectableTile>
+           )}
         </CoinGridStyled>}
     </AppContext.Consumer>
-}
+} 
