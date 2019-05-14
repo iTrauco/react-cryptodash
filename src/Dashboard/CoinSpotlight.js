cab906da845e476841e2d6620bd2ad792ffd4573
Christopher Trauco <chris@trau.co>
2019-05-14 15:39:02 -0400

    CoinSpotlight.js added for display in dashboard

---
+++ src/Dashboard/CoinSpotlight.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Tile} from '../Shared/Tile';
+import {AppContext} from '../App/AppProvider';
+import CoinImage from '../Shared/CoinImage';
+
+export default function () {
+    return <Tile> Hello Spotlight</Tile>
+}
