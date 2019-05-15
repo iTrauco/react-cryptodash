cda49f2fe98ea022599baabe5ca5242539bbe6de
Christopher Trauco <chris@trau.co>
2019-05-15 10:21:39 -0400

    added ChartSelect component for html styled dropdown of days, weeks, months, and years as units of measurements for time

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -4,13 +4,21 @@ import {Tile} from '../Shared/Tile';
 import{AppContext} from '../App/AppProvider';
 import ReactHighcharts from 'react-highcharts';
 import HighchartsTheme from './HighchartsTheme';
+import ChartSelect from './ChartSelect';
 ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
+
 
 export default function () {
     return (
         <AppContext.Consumer>
             {({historical}) =>
             <Tile>
+                <ChartSelect>
+                    <option value ='days'> Days </option>
+                    <option value ='days'> Weeks </option>
+                    <option value ='days'> Months </option>
+                    <option value ='days'> Years </option>
+                </ChartSelect>
                { historical ? <ReactHighcharts config={highchartsConfig(historical)}/>
                 : <div> Loading Historical Data</div>
             } 
