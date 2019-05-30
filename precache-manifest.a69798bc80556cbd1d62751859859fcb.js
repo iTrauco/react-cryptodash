b037ae8597c85e165d961f7a5ddad186a3292116
Christopher Trauco <chris@trau.co>
2019-05-30 03:01:28 -0400

    Updates

---
+++ precache-manifest.a69798bc80556cbd1d62751859859fcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94592a27879d6e3fd9e0051fe12e7bbf",
+    "url": "/react-cryptocoin-dashboard/index.html"
+  },
+  {
+    "revision": "a45ddec5481572675b1d",
+    "url": "/react-cryptocoin-dashboard/static/css/main.0e891e49.chunk.css"
+  },
+  {
+    "revision": "6e27ca4b9ce24f68cc9e",
+    "url": "/react-cryptocoin-dashboard/static/js/2.5202e90e.chunk.js"
+  },
+  {
+    "revision": "a45ddec5481572675b1d",
+    "url": "/react-cryptocoin-dashboard/static/js/main.20a81746.chunk.js"
+  },
+  {
+    "revision": "5d35e3baa807f76b0917",
+    "url": "/react-cryptocoin-dashboard/static/js/runtime~main.75120d50.js"
+  }
+]);
